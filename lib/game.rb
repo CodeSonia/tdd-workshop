9b729830109acbf339a6187d5179bd6e658b5caf
Sonia Hussain <sonia_ahm@icloud.com>
2023-10-23 11:03:52 +0100

    Test 1 pass: Rock beats Scissors

---
+++ lib/game.rb
@@ -0,0 +1,8 @@
+class RockPaperScissors
+
+  def play(player1, player2)
+    if player1 == 'rock' && player2 == 'scissors'
+      return 'Player 1 wins'
+    end
+  end
+end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,5 +1,15 @@
+require 'game'
+
 describe "Rspec" do
   it "is working" do
     expect(true).to eq(true)
+  end
+end
+
+describe RockPaperScissors do
+  let(:game) { RockPaperScissors.new }
+
+  it 'Rock beats Scissors' do
+    expect(game.play('rock', 'scissors')).to eq('Player 1 wins')
   end
 end
